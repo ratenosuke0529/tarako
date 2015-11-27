6833af971e024370e80528a3d7980630244047ed
小泉心之介 <shin200020529@gmail.com>
2015-11-27 20:09:05 +0900

    first commit

---
+++ オリジナルアプリ（仕分け）/ViewControllerd.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController　５.swift
+//  オリジナルアプリ（仕分け）
+//
+//  Created by 小泉心之介 on 2015/08/13.
+//  Copyright (c) 2015年 小泉心之介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController__: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
